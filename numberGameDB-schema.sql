4383e5842a2edb6f43b21b0ae715ced54ea65950
Austin Semple <aus824@gmail.com>
2022-04-24 14:37:15 -0400

    Update numberGameDB-schema.sql
    Added inserts for guesses. refactored a few things.

--- numberGameDB-schema.sql
+++ numberGameDB-schema.sql
@@ -17,6 +17,7 @@ FOREIGN KEY (statusId) REFERENCES `status`(statusId)
 
 CREATE TABLE guess(
 guessId INT PRIMARY KEY AUTO_INCREMENT,
+guessNumber INT NOT NULL,
 `time` TIMESTAMP NOT NULL,
 userGuess INT NOT NULL,
 result varchar(30) NOT NULL,
@@ -32,7 +33,12 @@ VALUES
 INSERT INTO game(answer, statusId)
 VALUES
     (1234, 1),
-    (2468, 1),
+    (2468, 2),
     (3579, 1);
 -- SELECT * FROM game;
 -- SELECT * FROM guess;
+INSERT INTO guess(guessNumber, time, userGuess, result, gameId)
+VALUES
+    (1, '2022-01-19 03:14:07', 1047, "testing", 1),
+    (2, '2022-01-19 03:11:06', 2183, "testing2", 1),
+    (1, '2022-02-11 03:01:07', 2468, "testing10", 2);
