1572cad91aba60b95e265520d5a855a0355f393b
Austin Semple <aus824@gmail.com>
2022-04-24 14:33:25 -0400

    Add files via upload
    Added GuessDto object mapper.
    Added impl's for getAllRounds, findById, and getAll.
    Changed some variable names.
    Added TODO's for what to work on during next meeting.
    
    Will need to look into guessNumber, datetimes, and displaying answer only when needed

--- GuessTheNumber/src/main/java/com/mycompany/guessthenumber/data/GuessTheNumberDatabaseDao.java
+++ GuessTheNumber/src/main/java/com/mycompany/guessthenumber/data/GuessTheNumberDatabaseDao.java
@@ -58,18 +58,20 @@ public class GuessTheNumberDatabaseDao implements GuessTheNumberDao {
             return statement;
 
         }, keyHolder);
-        
+
         gameDto.setGameId(keyHolder.getKey().intValue());
-        
+
         return gameDto;
     }
 
+    
+    //TODO : Update guessNumber to display which guess this is for the gameId the user is guessing for; --Austin
     @Override
     public GuessDto submitGuess(GuessDto guessDto) {
 
         //Timestamp for guess
         Date dt = new Date();
-        Timestamp databaseTimestamp = new Timestamp(dt.getTime());        
+        Timestamp databaseTimestamp = new Timestamp(dt.getTime());
 
         final String sqlGuess = "INSERT INTO guess (`time`, userGuess, result, roundId)"
                 + " VALUES(?, ?, ?, ?);";;
@@ -83,27 +85,35 @@ public class GuessTheNumberDatabaseDao implements GuessTheNumberDao {
             statement.setTimestamp(1, databaseTimestamp);
             statement.setString(2, Integer.toString(guessDto.getUserGuess()));
             statement.setString(3, guessDto.getResult());
-            statement.setString(4, Integer.toString(guessDto.getRoundId()));
+            statement.setString(4, Integer.toString(guessDto.getGuessId()));
             return statement;
 
         }, keyHolder);
 
-        
         return guessDto;
     }
 
+    //Retrieves all GameId's and info about the games
+    //TODO: Show answers for only games completed, and amount of guesses for each game
     @Override
     public List<GameDto> getAll() {
-        final String sql = "SELECT id, guessnumber, note, finished FROM guessnumber;";
+        final String sql = "select\n"
+                + "game.gameId, \n"
+                + "status.statusName,\n"
+                + "answer\n"
+                + "from game\n"
+                + "join status\n"
+                + "on game.statusId = status.statusId;";
         return jdbcTemplate.query(sql, new GuessGamerMapper());
     }
 
+    //Retrieves gameinfo given game Id
+    //TODO: Show answers only if the game is completed, amount of guesses for game
     @Override
     public GameDto findById(int id) {
 
-        final String sql = "SELECT id, guessnumber, note, finished "
-                + "FROM guessnumber WHERE id = ?;";
-
+        final String sql = "SELECT game.gameId, statusId, answer "
+                + "FROM game WHERE game.gameId = ?;";
         return jdbcTemplate.queryForObject(sql, new GuessGamerMapper(), id);
     }
 
@@ -124,29 +134,50 @@ public class GuessTheNumberDatabaseDao implements GuessTheNumberDao {
         return true;
     }
 
+    //Gets all rounds that have been played for a given gameId
     @Override
-    public boolean deleteById(int id) {
-        final String sql = "DELETE FROM guessnumber WHERE id = ?;";
-        return jdbcTemplate.update(sql, id) > 0;
+    public List<GuessDto> getAllRounds(int gameId) {
+
+        final String sql = "select \n"
+                + "guess.guessId,\n"
+                + "guess.time,\n"
+                + "guess.guessNumber,\n"
+                + "guess.userGuess,\n"
+                + "guess.result\n"
+                + "from guess\n"
+                + "where guess.gameId = ?\n"
+                + "order by guess.time;";
+
+        return jdbcTemplate.query(sql, new GuessGuessMapper(), gameId);
     }
 
-    @Override
-    public GameDto getGame(int gameId) {
-        final String sql = "SELECT gameId, statusId, answer "
-                + "FROM game WHERE gameId = ?;";
-
-        return jdbcTemplate.queryForObject(sql, new GuessGamerMapper(), gameId);
-    }
-
+    //Mapper for GameDto
     private static final class GuessGamerMapper implements RowMapper<GameDto> {
 
         @Override
         public GameDto mapRow(ResultSet rs, int index) throws SQLException {
             GameDto gameDto = new GameDto();
             gameDto.setGameId(rs.getInt("gameId"));
-            gameDto.setStatusName(rs.getString("statusId"));
+            gameDto.setStatusId(rs.getInt("statusId"));
             gameDto.setAnswer(rs.getInt("answer"));
             return gameDto;
         }
+    }
+
+    //Mapper for GuessDto 
+    private static final class GuessGuessMapper implements RowMapper<GuessDto> {
+
+        @Override
+        public GuessDto mapRow(ResultSet rs, int i) throws SQLException {
+            GuessDto guessDto = new GuessDto();
+            guessDto.setGuessId(rs.getInt("guessId"));
+            guessDto.setGuessNumber(rs.getInt("guessNumber"));
+            guessDto.setResult(rs.getString("result"));
+            guessDto.setTime(rs.getString("time"));
+            guessDto.setUserGuess(rs.getInt("userGuess"));
+
+            return guessDto;
+        }
+
     }
 }
