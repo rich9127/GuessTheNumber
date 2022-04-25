24031b0e67670e14cff1786e42f18e4fbcb61dae
Austin Semple <aus824@gmail.com>
2022-04-24 22:13:41 -0400

    Add files via upload
    Reorganizing vars

--- numberGameDB-schema.sql
+++ numberGameDB-schema.sql
@@ -3,23 +3,22 @@ CREATE DATABASE numberGameDB;
 
 USE numberGameDB;
 
-CREATE TABLE `status`(
+CREATE TABLE status(
 statusId INT PRIMARY KEY AUTO_INCREMENT,
 statusName VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE game(
 gameId INT PRIMARY KEY AUTO_INCREMENT,
-answer INT NOT NULL,
+answer varchar(6) NOT NULL,
 statusId INT NOT NULL,
-FOREIGN KEY (statusId) REFERENCES `status`(statusId)
+FOREIGN KEY (statusId) REFERENCES status(statusId)
 );
 
 CREATE TABLE guess(
 guessId INT PRIMARY KEY AUTO_INCREMENT,
-guessNumber INT NOT NULL,
 `time` TIMESTAMP NOT NULL,
-userGuess INT NOT NULL,
+userGuess varchar(6) NOT NULL,
 result varchar(30) NOT NULL,
 gameId INT NOT NULL,
 FOREIGN KEY (gameId) REFERENCES game(gameId)
@@ -37,8 +36,8 @@ VALUES
     (3579, 1);
 -- SELECT * FROM game;
 -- SELECT * FROM guess;
-INSERT INTO guess(guessNumber, time, userGuess, result, gameId)
+INSERT INTO guess(time, userGuess, result, gameId)
 VALUES
-    (1, '2022-01-19 03:14:07', 1047, "testing", 1),
-    (2, '2022-01-19 03:11:06', 2183, "testing2", 1),
-    (1, '2022-02-11 03:01:07', 2468, "testing10", 2);
+	('2022-01-19 03:14:07', 1047, "testing", 1),
+    ('2022-01-19 03:11:06', 2183, "testing2", 1),
+    ('2022-02-11 03:01:07', 2468, "testing10", 2);
