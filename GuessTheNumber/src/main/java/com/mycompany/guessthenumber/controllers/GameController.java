60301cf43b77873eb87ec5e6fe64b946830a6d45
Austin Semple <aus824@gmail.com>
2022-04-25 11:05:44 -0400

    Add files via upload
    Comments and service layer implementation

--- GuessTheNumber/src/main/java/com/mycompany/guessthenumber/controllers/GameController.java
+++ GuessTheNumber/src/main/java/com/mycompany/guessthenumber/controllers/GameController.java
@@ -7,6 +7,7 @@ package com.mycompany.guessthenumber.controllers;
 import com.mycompany.guessthenumber.data.GuessTheNumberDao;
 import com.mycompany.guessthenumber.models.GameDto;
 import com.mycompany.guessthenumber.models.GuessDto;
+import com.mycompany.guessthenumber.service.GameServiceLayerImpl;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,106 +30,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/game")
 public class GameController {
 
-    /*
-    "begin" - POST – Starts a game, generates an answer, and sets the correct 
-    status. Should return a 201 CREATED message as well as the created gameId.
-    
-    "guess" – POST – Makes a guess by passing the guess and gameId in as JSON. 
-    The program must calculate the results of the guess and mark the game 
-    finished if the guess is correct. It returns the Round object with the results filled in.
-    
-    "game" – GET – Returns a list of all games. Be sure in-progress games 
-    
-    do not display their answer.
-    "game/{gameId}" - GET – Returns a specific game based on ID. 
-    Be sure in-progress games do not display their answer.
-    
-    "rounds/{gameId} – GET – Returns a list of rounds for the specified 
-    game sorted by time.
-
-     */
-    //Create temporary GameDto object for testing
-//    GameDto tempGameDto = new GameDto();
+    private final GameServiceLayerImpl service;
     private final GuessTheNumberDao dao;
-
-    public GameController(GuessTheNumberDao dao) {
+    public GameController(GameServiceLayerImpl service, GuessTheNumberDao dao) {
+        this.service = service;
         this.dao = dao;
     }
 
     @PostMapping("/begin")
     @ResponseStatus(HttpStatus.CREATED)
     public GameDto createGame(GameDto gameDto) {
-
-        List<Integer> randomArr = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        
-        //To be transferred to the service layer --BEGIN
-        Random rand = new Random();
-        StringBuilder builder = new StringBuilder();
-        for(int i=0; i<4; i++){
-            Integer temp = rand.nextInt(randomArr.size());
-            builder.append(randomArr.get(temp));
-            randomArr.remove(temp);
-        }
-       
-        gameDto.setAnswer(builder.toString());
-        gameDto.setStatusId(1);
-        //To be transferred to the service layer --END
-
-        dao.addGame(gameDto);
-        gameDto.setAnswer("????");
-        return gameDto;
+        return service.createGame(gameDto);
     }
 
     @PostMapping("/guess/{gameId}")
     public GuessDto create(@PathVariable int gameId, @RequestBody GuessDto guessDto) {
-        //To be transferred to the service layer --BEGIN
-        int correctDigits = 0;
-        int correctPlaces = 0;
-
-        String userGuessString = guessDto.getUserGuess();
-        String gameAnswerString = dao.findById(gameId).getAnswer();
-
-        //Will cause error if the user guess is not the correct length
-        if (userGuessString.equals(gameAnswerString)) {
-            guessDto.setResult("All Correct");
-            dao.setGameFinish(gameId);
-        } else {
-            for (int i = 0; i < gameAnswerString.length(); i++) {
-                if (userGuessString.charAt(i) == gameAnswerString.charAt(i)) {
-                    correctPlaces++;
-                }
-
-                for (int j = 0; j < gameAnswerString.length(); j++) {
-                    if (userGuessString.charAt(i) == gameAnswerString.charAt(j)) {
-                        correctDigits++;
-                    }
-                }
-            }
-
-            guessDto.setResult("E:" + correctDigits + " P:" + correctPlaces);
-        }
-        //To be transferred to the service layer --END
-        guessDto.setGameId(gameId);
-        return dao.submitGuess(guessDto);
+        return service.create(gameId, guessDto);
     }
     
     @GetMapping
     public List<GameDto> getAll() {
-        List<GameDto> gameDtoList = dao.getAll();
-        for(GameDto obj : gameDtoList){
-            if(obj.getStatusId() == 1){
-                obj.setAnswer("????");
-            } else {
-                continue;
-            }
-        }
-        return gameDtoList;
+        return service.getAll();
     }
     
     @GetMapping("/{id}")
     public ResponseEntity<GameDto> findById(@PathVariable int id) {
-        GameDto result = dao.findById(id);
-        result.setAnswer("????");
+        GameDto result = service.findById(id);
+        if(result.getStatusId()==1){
+            result.setAnswer("????");
+        }
         if (result == null) {
             return new ResponseEntity(null, HttpStatus.NOT_FOUND);
         }
@@ -137,7 +67,7 @@ public class GameController {
     
     @GetMapping("rounds/{id}")
     public ResponseEntity<List<GuessDto>> getAllRounds(@PathVariable int id) {
-        List<GuessDto> result = new ArrayList<>(dao.getAllRounds(id));
+        List<GuessDto> result = new ArrayList<>(service.getAllRounds(id));
         if (result == null) {
             return new ResponseEntity(null, HttpStatus.NOT_FOUND);
         }
